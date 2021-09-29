6debfed82f91aadf2c3b7147959e49613b8d7063
fongzhizhi <1837486996@qq.com>
2021-09-29 21:27:03 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
+import Vue  from 'vue/dist/vue.common.dev'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = document.createElement('div');
+app.id = 'app';
+app.innerHTML = '<App>{{ msg }}</App>'
+document.body.append(app)
+
+new Vue({
+    el: '#app',
+    data: {
+        msg: 'THis is a message'
+    },
+    components: {
+        app: App
+    }
+})
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+// webpack.config.js
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  /**模式 */
+  mode: 'development',
+  /**入口 */
+  entry: path.resolve(__dirname, './src/main.js'),
+  /**出口 */
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  /**构建目标 */
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // 它会应用到普通的 `.js` 文件
+      // 以及 `.vue` 文件中的 `<script>` 块
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // 它会应用到普通的 `.css` 文件
+      // 以及 `.vue` 文件中的 `<style>` 块
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              limit: 1024,
+              esModule: false
+            }
+          }
+        ]
+      }
+    ]
+  },
+  /**插件 */
+  plugins: [
+    new HtmlWebpackPlugin(),
+    // 请确保引入这个插件来施展魔法
+    new VueLoaderPlugin()
+  ],
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    concatenateModules: true,
+  },
+  /**服务 */
+  devServer: {
+    // contentBase: [
+    //     path.join(__dirname, 'dist'),
+    //     path.join(__dirname, 'pybulic'),
+    // ],
+    // watchContentBase: true,
+    compress: false, 
+    port: 9000,
+    hot: true,
+    open: false,
+  }
+}
